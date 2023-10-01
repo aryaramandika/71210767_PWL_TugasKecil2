8362b29d1af58e1d8691cd5f3d5518a82e0a3611
aryaramandika <93310899+aryaramandika@users.noreply.github.com>
2023-10-01 23:02:39 +0700

    Add files via upload

--- env.js
+++ env.js
@@ -1,12 +1,15 @@
-// env.js
+// 1. Membuat Env Variable sesuai data yang diregister
 const cat_env = {
-  API_KEY: "live_tOqt81j977NpFfd8xyofyUzbefy3Qurr948vvE7zqbm1MZGgp3oaG5xyS16kgi86",
-  endpoint: "https://api.thecatapi.com/",
+    API_KEY:
+        "",
+    endpoint: "https://api.thecatapi.com/",
 };
 
 const dog_env = {
-  API_KEY: "live_tOqt81j977NpFfd8xyofyUzbefy3Qurr948vvE7zqbm1MZGgp3oaG5xyS16kgi86",
-  endpoint: "https://api.thedogapi.com/",
+    API_KEY:
+        "live_tOqt81j977NpFfd8xyofyUzbefy3Qurr948vvE7zqbm1MZGgp3oaG5xyS16kgi86",
+    endpoint: "https://api.thedogapi.com/",
 };
 
-export { cat_env, dog_env };
+// 2. Export dog_env
+export { dog_env, cat_env };
